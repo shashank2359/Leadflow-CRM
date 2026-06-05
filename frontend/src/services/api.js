498bf04cfdde9063d3b6a9c97e35e2396b93b66d
Shashank Singh Sengar <shashanksengar238@gmail.com>
2026-06-05 12:24:52 +0530

    Production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://leadflow-crm-ekxa.onrender.com/api",
 });
 
-// Automatically attach JWT token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
